92a1eb8f6a713a1031b47883765ec5ee2f7955f4
fauzi <fauzibinfaisal@gmail.com>
2018-12-20 14:47:07 +0700

    Initial commit

---
+++ app/src/main/java/com/fauzify/firebaseloginkotlin/AnonymousActivity.kt
@@ -0,0 +1,12 @@
+package com.fauzify.firebaseloginkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AnonymousActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anonymous)
+    }
+}
---
+++ app/src/main/java/com/fauzify/firebaseloginkotlin/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package com.fauzify.firebaseloginkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        
+    }
+}
